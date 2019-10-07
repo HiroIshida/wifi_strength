65d4f58dc63f81acb9e235eb9a42ff4b45f8c6a7
HiroIshida <spitfire.docomo@gmail.com>
2019-10-07 17:22:38 +0900

    fix regex

--- main.py
+++ main.py
@@ -5,21 +5,26 @@ import subprocess
 import re
 from std_msgs.msg import Float32
 
-rospy.init_node('wifi_strength_publisher')
 
 def get_wifi_strength():
     result_ = subprocess.check_output(["iwconfig"], stderr = subprocess.STDOUT)
     result = ''.join(result_)
-    tmp = re.findall(r'level=-\d\d', result)[0]
+    tmp = re.findall(r'level=-\d+', result)[0]
     wifi_strength = float(tmp[-3:])
     return wifi_strength
 
+def main():
+    rospy.init_node('wifi_strength_publisher')
+    pub = rospy.Publisher("wifi_strength", Float32, queue_size = 1)
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        wifi_strength = get_wifi_strength()
+        msg = Float32(data = wifi_strength)
+        pub.publish(msg)
 
-pub = rospy.Publisher("wifi_strength", Float32, queue_size = 1)
-r = rospy.Rate(10)
-while not rospy.is_shutdown():
-    msg = Float32(data = get_wifi_strength())
-    pub.publish(msg)
+main()
+
+
 
 
 
