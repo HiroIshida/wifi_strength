3dd1f8559de051eef084b2bf6f68c71b287db3b1
HiroIshida <spitfire.docomo@gmail.com>
2019-10-13 01:21:47 +0900

    tf base -> map

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import subprocess
 import re
 from std_msgs.msg import Float32
+import tf
 
 
 def get_wifi_strength():
@@ -16,8 +17,15 @@ def get_wifi_strength():
 def main():
     rospy.init_node('wifi_strength_publisher')
     pub = rospy.Publisher("wifi_strength", Float32, queue_size = 1)
+    listener = tf.TransformListener()
+    listener.waitForTransform("/map", "/base_link", rospy.Time(), rospy.Duration(4.0))
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
+        now = rospy.Time.now()
+        listener.waitForTransform("/map", "/base_link", now, rospy.Duration(3.0))
+        trans, rot = listener.lookupTransform("/map", "/base_link", rospy.Time((0)))
+        print trans
+
         wifi_strength = get_wifi_strength()
         msg = Float32(data = wifi_strength)
         pub.publish(msg)
