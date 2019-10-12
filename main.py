9a682eef0a6f7f97c4984d5444ab648ae8b418c2
HiroIshida <spitfire.docomo@gmail.com>
2019-10-13 03:17:32 +0900

    devide one function to two

--- main.py
+++ main.py
@@ -19,19 +19,30 @@ def callback_map(msg):
 
     msg_map_zeros = copy.deepcopy(msg)
 
-def position2mapidx(trans):
+def get_global_position(trans):
     global msg_map
-    global msg_map_zeros
-    if msg_map is None or msg_map_zeros is None:
+    if msg_map is None:
         return None
-    info = msg_map.info
-    resol = info.resolution
 
-
-    pos_map = info.origin.position
-    data = [0 for i in range(len(msg_map_zeros.data))]
+    pos_map = msg_map.info.origin.position
     x = - pos_map.x + trans[0]
     y = - pos_map.y + trans[1]
+    return x, y
+
+def create_debug_map_message(trans):
+    ret = get_global_position(trans)
+    if ret is None:
+        return None
+    x = ret[0]
+    y = ret[1]
+
+    global msg_map_zeros
+    if msg_map_zeros is None:
+        return None
+
+    info = msg_map_zeros.info
+    resol = info.resolution
+    data = [0 for i in range(len(msg_map_zeros.data))]
     idx_x = int(x // resol)
     idx_y = int(y // resol)
     to_idx = lambda i, j: j * info.height + i
@@ -41,7 +52,6 @@ def position2mapidx(trans):
             data[idx] = 1.0
     msg_map_zeros.data = data
     return msg_map_zeros
-
 
 def get_wifi_strength():
     result_ = subprocess.check_output(["iwconfig"], stderr = subprocess.STDOUT)
@@ -63,7 +73,7 @@ def main():
         now = rospy.Time.now()
         listener.waitForTransform("/map", "/base_link", now, rospy.Duration(3.0))
         trans, rot = listener.lookupTransform("/map", "/base_link", rospy.Time((0)))
-        msg = position2mapidx(trans)
+        msg = create_debug_map_message(trans)
 
         """
         wifi_strength = get_wifi_strength()
