d20a72204807bc1e492c1996acc77a79a76ed38b
HiroIshida <spitfire.docomo@gmail.com>
2019-10-13 03:56:41 +0900

    use class

--- main.py
+++ main.py
@@ -8,65 +8,60 @@ from std_msgs.msg import Float32
 from nav_msgs.msg import OccupancyGrid
 import tf
 
-global msg_map
-global msg_map_zeros
-msg_map = None
-msg_map_zeros = None
+class WifiMap:
+    def __init__(self):
+        self.listener = tf.TransformListener()
+        self.listener.waitForTransform("/map", "/base_link", rospy.Time(), rospy.Duration(4.0))
+        self.sub = rospy.Subscriber("map", OccupancyGrid, self._callback_map)
+        self.map = None
+        self.map_copied = None
 
-def callback_map(msg):
-    global msg_map, msg_map_zeros
-    msg_map = msg
+    def _callback_map(self, msg_map):
+        self.map = msg_map
+        self.map_copied = copy.deepcopy(msg_map)
 
-    msg_map_zeros = copy.deepcopy(msg)
+    def get_global_position(self):
+        if self.map is None:
+            return None
 
-def get_global_position(trans):
-    global msg_map
-    if msg_map is None:
-        return None
+        now = rospy.Time.now()
+        self.listener.waitForTransform("/map", "/base_link", now, rospy.Duration(3.0))
+        trans, _ = self.listener.lookupTransform("/map", "/base_link", rospy.Time((0)))
+        pos_map_origin = self.map.info.origin.position
+        x = - pos_map_origin.x + trans[0]
+        y = - pos_map_origin.y + trans[1]
+        return x, y
 
-    pos_map = msg_map.info.origin.position
-    x = - pos_map.x + trans[0]
-    y = - pos_map.y + trans[1]
-    return x, y
+    def create_debug_map(self):
+        ret = self.get_global_position()
+        if ret is None:
+            return None
+        x, y = ret
 
-def create_debug_map_message(trans):
-    ret = get_global_position(trans)
-    if ret is None:
-        return None
-    x = ret[0]
-    y = ret[1]
+        if self.map_copied is None:
+            return None
 
-    global msg_map_zeros
-    if msg_map_zeros is None:
-        return None
-
-    info = msg_map_zeros.info
-    resol = info.resolution
-    data = [0 for i in range(len(msg_map_zeros.data))]
-    idx_x = int(x // resol)
-    idx_y = int(y // resol)
-    to_idx = lambda i, j: j * info.height + i
-    for i in [idx_x - 50 + i for i in range(101)]:
-        for j in [idx_y - 50 + j for j in range(101)]:
-            idx = to_idx(i, j)
-            data[idx] = 1.0
-    msg_map_zeros.data = data
-    return msg_map_zeros
+        info = self.map_copied.info
+        resol = info.resolution
+        data = [0 for i in range(len(self.map_copied.data))]
+        idx_x = int(x // resol)
+        idx_y = int(y // resol)
+        to_idx = lambda i, j: j * info.height + i
+        for i in [idx_x - 50 + i for i in range(101)]:
+            for j in [idx_y - 50 + j for j in range(101)]:
+                idx = to_idx(i, j)
+                data[idx] = 1.0
+        self.map_copied.data = data
+        return self.map_copied
 
 def main():
     rospy.init_node('wifimap_publisher')
     pub_map = rospy.Publisher("wifimap", OccupancyGrid, queue_size = 1)
-
-    rospy.Subscriber("map", OccupancyGrid, callback_map)
-    listener = tf.TransformListener()
-    listener.waitForTransform("/map", "/base_link", rospy.Time(), rospy.Duration(4.0))
+    wfm = WifiMap()
     r = rospy.Rate(1)
     while not rospy.is_shutdown():
-        now = rospy.Time.now()
-        listener.waitForTransform("/map", "/base_link", now, rospy.Duration(3.0))
-        trans, rot = listener.lookupTransform("/map", "/base_link", rospy.Time((0)))
-        msg = create_debug_map_message(trans)
-        if msg_map_zeros is not None:
+        msg = wfm.create_debug_map()
+        if msg is not None:
             pub_map.publish(msg)
         r.sleep()
 
